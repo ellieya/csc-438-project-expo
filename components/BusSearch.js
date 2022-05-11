f63aa679bf8294c5810c2b1250edfe90ddf3cc0c
Ellie <j.chen.ellie@gmail.com>
2022-05-10 21:12:22 -0400

    last of what was completed

--- components/BusSearch.js
+++ components/BusSearch.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
 import { Text, View, Button, TextInput } from 'react-native';
 import { TouchableOpacity } from 'react-native-web';
+import Modal from "react-native-modal";
+
+function BusStopDisplayer(props) {
+    console.log({"el boos": props.stopGroupList});
+    let stopLists = [];
+
+    for (let i = 0; i < props.stopGroupList.length; i++) {
+        stopLists[i] = [];
+        for (let j = 0; j < props.stopGroupList[i].stopIds.length; j++) {
+            stopLists[i].push(<Text key={i + j + props.stopGroupList[i].stopIds[j]}>{props.stopGroupList[i].stopIds[j]}</Text>)
+        }
+    }
+
+    return(<View>
+        {stopLists[0]}
+    </View>)
+}
 
 export class BusSearch extends Component {
+
 
     constructor(props) {
         super(props);
 
         this.state = {
-            loading: true
+            loading: true,
+            isModalVisible: false,
+            modalLoading: true
         }
 
         this.allBusObjects = [];
         this.displayedBusObjects;
+        this.busStopGroups = [];
     }
 
     async getAllBusRoutes() {
@@ -22,9 +43,9 @@ export class BusSearch extends Component {
                 let i = 0;
                 for (let prop in json) {
                     this.allBusObjects.push(
-                        <TouchableOpacity onPress={this.handleBusTapped} key={i + prop} name={prop}>
+                        <TouchableOpacity onPress={e => this.handleBusTapped(json[prop].id[0])} key={i + prop} name={prop}>
                             <View>
-                            <Text >{prop} - {json[prop].longName[0]}</Text>
+                                <Text >{prop} - {json[prop].longName[0]}</Text>
                             </View>
                         </TouchableOpacity>
                     );
@@ -35,8 +56,18 @@ export class BusSearch extends Component {
             });
     }
 
-    handleBusTapped = () => {
-        console.log("bus has been tapped");
+    handleBusTapped = async (busId) => {
+        this.toggleModal();
+        await fetch(`https://us-central1-csc438-project.cloudfunctions.net/app/bus/all_route_stops/${busId}`)
+        .then(data => data.json())
+        .then(json => {
+            this.busStopGroups = json;
+            this.setState({modalLoading: false});
+        });
+    }
+
+    toggleModal = () => {
+        this.setState({isModalVisible: !this.state.isModalVisible});
     }
 
     updateDisplayedBusObjects = (value) => {
@@ -47,8 +78,6 @@ export class BusSearch extends Component {
         for (let i = 0; i < this.allBusObjects.length; i++) {
             let matchingCase = true;
             for (let j = 0; j < value.length; j++) {
-                console.log("this.allBusObjects[i].name[j] = " + this.allBusObjects[i].props.name[j]);
-                console.log("value[j] = " + value[j]);
                 if (this.allBusObjects[i].props.name[j] !== value[j]) {
                     matchingCase = false;
                 }
@@ -79,6 +108,14 @@ export class BusSearch extends Component {
         else {
             return (
                 <View>
+                    <View>
+                        <Modal isVisible={this.state.isModalVisible} backdropColor="white">
+                            <View style={{ flex: 1 }}>
+                                <Text>{this.state.modalLoading ? "Loading" : <BusStopDisplayer stopGroupList={this.busStopGroups}/>}</Text>
+                                <Button value="Close" onPress={this.toggleModal}></Button>
+                            </View>
+                        </Modal>
+                    </View>
                     <TextInput onChange={e => this.updateDisplayedBusObjects(e.target.value)}></TextInput>
                     {this.displayedBusObjects}
                 </View>
